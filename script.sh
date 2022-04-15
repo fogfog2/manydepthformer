2e0c112b44d9cb14ed630263553d13fbc3e73a92
fogfog2 <s-eungju-n@hanmail.net>
2022-04-15 15:29:54 +0900

    22

--- script.sh
+++ script.sh
@@ -1,17 +1,3 @@
 export PYTHONPATH="${PYTHONPATH}:/home/sj/src/manydepthformer" 
 
-python manydepth/train.py --data_path=/home/sj/kitti --log_dir=/media/sj/data/manydepth/kitti_cmt_l2_b8 --batch_size=8 --width=640 --height=192 --num_epochs=20 --freeze_teacher_epoch=15 --scheduler_step_size=5 --cmt_layer=2 --cmt_dim=64 --train_mode=cmt --png
-
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l2_b8/mdp/models/weights_18 --eval_mono --cmt_layer=2 --cmt_dim=64 --png
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l2_b8/mdp/models/weights_19 --eval_mono --cmt_layer=2 --cmt_dim=64 --png
-
-python manydepth/train.py --data_path=/home/sj/kitti --log_dir=/media/sj/data/manydepth/kitti_cmt_l3_b8 --batch_size=8 --width=640 --height=192 --num_epochs=20 --freeze_teacher_epoch=15 --scheduler_step_size=5 --cmt_layer=3 --cmt_dim=64 --train_mode=cmt --png
-
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l3_b8/mdp/models/weights_18 --eval_mono --cmt_layer=3 --cmt_dim=64 --png
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l3_b8/mdp/models/weights_19 --eval_mono --cmt_layer=3 --cmt_dim=64 --png
-
-
-python manydepth/train.py --data_path=/home/sj/kitti --log_dir=/media/sj/data/manydepth/kitti_cmt_l4_b8 --batch_size=8 --width=640 --height=192 --num_epochs=20 --freeze_teacher_epoch=15 --scheduler_step_size=5  --cmt_layer=4 --cmt_dim=64 --train_mode=cmt --png
-
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l4_b8/mdp/models/weights_18 --eval_mono --cmt_layer=4 --cmt_dim=64 --png
-python manydepth/evaluate_depth_2.py --data_path=/home/sj/kitti --load_weights_folder=/media/sj/data/manydepth/kitti_cmt_l4_b8/mdp/models/weights_19 --eval_mono --cmt_layer=4 --cmt_dim=64 --png
+python manydepth/train.py --data_path=/home/sj/Documents/cityscapes_preprocessed --log_dir=/home/sj/manydepth/city_cmt_22_attention_s2_r01_64k_r1_adam --dataset=cityscapes_preprocessed --split=cityscapes_preprocessed --batch_size=8 --width=512 --height=192 --num_epochs=40 --scheduler_step_size=2 --scheduler_step_ratio=0.1 --pytorch_random_seed=1 --cmt_layer=3 --freeze_teacher_step=64000 --train_mode=cmt --use_attention_decoder --png
